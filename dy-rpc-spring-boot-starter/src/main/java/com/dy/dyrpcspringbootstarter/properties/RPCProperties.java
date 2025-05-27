936d1b63e15214282673d01db4e81f92fd16f531
dy302 <3021520010@qq.com>
2025-05-27 10:42:10 +0800

    删除测试的代码

--- dy-rpc-spring-boot-starter/src/main/java/com/dy/dyrpcspringbootstarter/properties/RPCProperties.java
+++ dy-rpc-spring-boot-starter/src/main/java/com/dy/dyrpcspringbootstarter/properties/RPCProperties.java
@@ -13,9 +13,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
-import java.rmi.registry.Registry;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @Data
 @ConfigurationProperties(prefix = "rpc")
